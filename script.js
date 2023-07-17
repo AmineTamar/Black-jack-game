d38131f3922755380f32982133b0e466a04f16ad
AmineTamar <101942943+AmineTamar@users.noreply.github.com>
2023-07-17 23:26:23 +0000

    practice

--- script.js
+++ script.js
@@ -1,37 +1,125 @@
 
 
+  
+ 
 
-  let firstCard =6;
-  let secondCard=9;
-  let sum = firstCard + secondCard +6;
+  let cards =[];
+  let sum = 0 
   let hasBlackJack = false;
-  let isAlive = true;
+  let isAlive = false;
   let gamemessage = document.getElementById("message")
   let sumEl =document.querySelector("#sum-el")
+  let cardsEl = document.getElementById("cards")
+  let playerEl = document.getElementById("player")
+
+  let player ={
+     name :"Amine",
+     chips :0
+
+  }
 
 
-  function startGame(){
+  playerEl.textContent = `${player.name} : ${player.chips}$`;
+ 
+
+
+
+
+  function getRandomCard(){
+
+    let randomNum = Math.floor(Math.random()*13)+1; 
+  
+    if(randomNum === 1) {
+            
+          return 11;
+    }
+    else if (randomNum>10 && randomNum<14){ 
+      return 10;
+  
+  }
+  else {
+    return randomNum
+  }
+  }
+  
+
+function startGame (){
+
+isAlive=true
+
+let firstCard =getRandomCard();
+let secondCard=getRandomCard();
+ 
+  cards = [firstCard,secondCard]
+   sum= firstCard + secondCard
+
+   
+  renderGame();
+ 
+}
+
+
+
+
+  function renderGame(){
+
+    
+      cardsEl.innerHTML = "Cards: "; // Clear the previous card display
+
+      for(let i = 0 ; i<cards.length;i++){
+      cardsEl.innerHTML += cards[i] +" ";
+      } 
+
+      sumEl.textContent= `Sum: ${sum}`;
 
 if ( sum <= 20 ){
-  
-  gamemessage.innerHTML = " do u want to Draw a new card ?";
 
-  ;
-  isAlive=true;
+  
+  
+  gamemessage.textContent = " Do u want to Draw a new card ?";
+
+  
+ 
 
 }
 else if (sum === 21){
 
-  gamemessage.innerHTML = "you won";
+  gamemessage.textContent = "you won";
 
+  
+  player.chips++;
+  playerEl.textContent = `${player.name} : ${player.chips}$`;
   hasBlackJack = true;
+
+  /*credits++;
+  creditsEl.innerHTML+=`Credits : ${credits}` */
+  
 }
 else {
-  gamemessage.innerHTML = " you lost";
+  gamemessage.textContent = " you lost";
 
   
   isAlive=false;
 }
 
-sumEl.innerHTML= `sum :${sum}`;
+
+
   }
+
+
+  function newCard(){
+
+    if (isAlive===true && hasBlackJack===false){
+      let nCard = getRandomCard();
+      sum = sum + nCard;
+      cards.push(nCard);
+      renderGame();
+    }
+  }
+    
+    
+    
+
+
+  
+

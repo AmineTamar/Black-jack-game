97e9d2098e3bdca5e7d979caded0a9ff9c496909
AmineTamar <101942943+AmineTamar@users.noreply.github.com>
2023-07-17 23:50:39 +0000

    added dollar counter

--- script.js
+++ script.js
@@ -3,20 +3,23 @@
   
  
 
-  let cards =[];
-  let sum = 0 
-  let hasBlackJack = false;
-  let isAlive = false;
-  let gamemessage = document.getElementById("message")
-  let sumEl =document.querySelector("#sum-el")
-  let cardsEl = document.getElementById("cards")
-  let playerEl = document.getElementById("player")
-
   let player ={
      name :"Amine",
      chips :0
 
   }
+  let cards =[];
+  let sum = 0 
+  /*let hasBlackJack = false;
+  let isAlive = false;*/
+
+  
+  let gamemessage = document.getElementById("message")
+  let sumEl =document.querySelector("#sum-el")
+  let cardsEl = document.getElementById("cards")
+  let playerEl = document.getElementById("player")
+  let gameOver = false;
+
 
 
   playerEl.textContent = `${player.name} : ${player.chips}$`;
@@ -45,8 +48,8 @@
 
 function startGame (){
 
-isAlive=true
-
+/*isAlive=true */
+gameOver = false;
 let firstCard =getRandomCard();
 let secondCard=getRandomCard();
  
@@ -61,55 +64,28 @@ let secondCard=getRandomCard();
 
 
 
-  function renderGame(){
-
-    
-      cardsEl.innerHTML = "Cards: "; // Clear the previous card display
-
-      for(let i = 0 ; i<cards.length;i++){
-      cardsEl.innerHTML += cards[i] +" ";
-      } 
-
-      sumEl.textContent= `Sum: ${sum}`;
-
-if ( sum <= 20 ){
-
-  
-  
-  gamemessage.textContent = " Do u want to Draw a new card ?";
-
-  
- 
-
-}
-else if (sum === 21){
-
-  gamemessage.textContent = "you won";
-
-  
-  player.chips++;
-  playerEl.textContent = `${player.name} : ${player.chips}$`;
-  hasBlackJack = true;
-
-  /*credits++;
-  creditsEl.innerHTML+=`Credits : ${credits}` */
-  
-}
-else {
-  gamemessage.textContent = " you lost";
-
-  
-  isAlive=false;
-}
-
-
-
+function renderGame() {
+  cardsEl.innerHTML = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.innerHTML += cards[i] + " ";
   }
-
+  sumEl.textContent = `Sum: ${sum}`;
+  if (sum <= 20) {
+    gamemessage.textContent = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    gamemessage.textContent = "You won!";
+    player.chips++;
+    playerEl.textContent = `${player.name} : ${player.chips}$`;
+    gameOver = true;
+  } else {
+    gamemessage.textContent = "You lost!";
+    gameOver = true;
+  }
+}
 
   function newCard(){
 
-    if (isAlive===true && hasBlackJack===false){
+    if (gameOver===false){
       let nCard = getRandomCard();
       sum = sum + nCard;
       cards.push(nCard);
